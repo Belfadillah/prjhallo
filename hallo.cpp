d29dc572472d6a592d0623c7c8b549826f403656
Belfadillah <mbelfadillahf@gmail.com>
2025-06-12 12:17:03 +0800

    Menampilkan data pada array

--- hallo.cpp
+++ hallo.cpp
@@ -1,17 +1,23 @@
+// Commit: Menampilkan isi array string ke layar
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
-    // Inisialisasi array string untuk 5 nama
     string nama[5];
 
-    // Memasukkan 5 nama
     nama[0] = "Andi";
     nama[1] = "Budi";
     nama[2] = "Citra";
     nama[3] = "Dewi";
     nama[4] = "Eko";
+
+    // Menampilkan isi array
+    cout << "Daftar Nama:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << nama[i] << endl;
+    }
 
     return 0;
 }
